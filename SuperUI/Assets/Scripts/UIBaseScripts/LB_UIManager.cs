a7e8bb749dacf7202873e7ee514a5e1504778089
Halil Coşgun <cosgun.halil@gmail.com>
2018-08-09 15:14:32 +0300

    Base UI Scripts are added
    LB_UIManager
    LB_UIPanel
    LB_UIEventArgs
    LB_UIEnumContainer

---
+++ SuperUI/Assets/Scripts/UIBaseScripts/LB_UIManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LB_UIManager : MonoBehaviour {
+
+    private static LB_UIManager instance;
+    public static LB_UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (LB_UIManager)FindObjectOfType(typeof(LB_UIManager));
+                instance.Init();
+            }
+
+            return instance;
+        }
+    }
+
+    public delegate void ActivatePanelDelegate(LB_UIEventArgs eventArgs);
+    public event ActivatePanelDelegate PanelActivated;
+
+    protected Dictionary<PanelType, LB_UIPanel> panelsByPanelType;
+
+    public PanelType CurrentPanel;
+    protected LB_UIPanel _lastActivePanel;
+
+    protected bool _isPopUpActivate;
+
+    public virtual void Init()
+    {
+
+    }
+
+    public virtual void InitPanels()
+    {
+        
+    }
+
+    public virtual void ActivatePanel(LB_UIEventArgs eventArgs)
+    {
+        if (PanelActivated != null)
+        {
+            PanelActivated(eventArgs);
+        }
+    }
+
+    public virtual void DeactivatePanel(PanelType panel)
+    {
+
+    }
+
+    public virtual void DeactivateLastActivePanel()
+    {
+
+    }
+
+    public virtual void PopUpActivated(bool isActivated)
+    {
+
+    }
+
+    public virtual void ShowPopUpMessage(string message)
+    {
+
+    }
+}
