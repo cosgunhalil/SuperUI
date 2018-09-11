0ed6d60affa66c0380cf96a5718ebe8362556534
Halil Coşgun <cosgun.halil@gmail.com>
2018-09-11 12:11:55 +0300

    UIElement and UIPanel is completed

--- SuperUI/Assets/Scripts/UIBaseScripts/LB_UIElement.cs
+++ SuperUI/Assets/Scripts/UIBaseScripts/LB_UIElement.cs
@@ -1,16 +1,120 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LB_UIElement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    protected RectTransform UIElementRectTransform;
+    protected Vector2 ActivatedCoordinate;
+    protected Vector2 DeactivtedCoordinate;
+
+    public virtual void Init()
+    {
+        UIElementRectTransform = GetComponent<RectTransform>();
+    }
+
+    public virtual void PlayActivateAnimation()
+    {
+
+    }
+
+    public virtual void PlayActivateAnimation(float delay)
+    {
+
+    }
+
+    public virtual void PlayDeactivateAnimation()
+    {
+
+    }
+
+    public virtual void PlayDeactivateAnimation(float delay)
+    {
+
+    }
+
+    public virtual void PlaySelectionAnimation()
+    {
+
+    }
+
+    public virtual void PlayUnselectAnimation()
+    {
+
+    }
+
+    public virtual void SetSize(Vector2 size)
+    {
+        
+    }
+
+    public virtual void CalculateCoordinates()
+    {
+
+    }
+
+    public virtual void CalculateCoordinates(float canvasWidth)
+    {
+
+    }
+
+    public virtual void CalculateCoordinates(Vector2 screenSize)
+    {
+
+    }
+
+    public virtual void SetPosition(float width)
+    {
+
+    }
+
+    public void SetAnchorMax(Vector2 anchorMax)
+    {
+        if (UIElementRectTransform != null)
+        {
+            UIElementRectTransform.anchorMax = anchorMax;
+        }
+    }
+
+    public void SetAnchorMin(Vector2 anchorMin)
+    {
+        if (UIElementRectTransform != null)
+        {
+            UIElementRectTransform.anchorMin = anchorMin;
+        }
+    }
+
+    public void SetAnchorPosition(Vector2 position)
+    {
+        if (UIElementRectTransform != null)
+        {
+            UIElementRectTransform.anchoredPosition = position;
+        }
+    }
+
+    public RectTransform GetRectTransform()
+    {
+        if (UIElementRectTransform == null)
+        {
+            UIElementRectTransform = GetComponent<RectTransform>();
+        }
+
+        return UIElementRectTransform;
+    }
+
+    public virtual void CalculateSize(Vector2 screenSize)
+    {
+
+    }
+
+    public virtual void StartIdleAnimation()
+    {
+
+    }
+
+    public virtual void StopIdleAnimation()
+    {
+
+    }
 }
--- SuperUI/Assets/Scripts/UIBaseScripts/LB_UIPanel.cs
+++ SuperUI/Assets/Scripts/UIBaseScripts/LB_UIPanel.cs
@@ -4,13 +4,64 @@ using UnityEngine;
 
 public class LB_UIPanel : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public LB_UIElement[] UIElements;
+
+    protected Canvas PanelCanvas;
+    protected RectTransform PanelRectTransform;
+
+    protected float AnimationTime;
+
+    public virtual void Init()
+    {
+        AnimationTime = .5f;
+        PanelCanvas = GetComponent<Canvas>();
+        PanelRectTransform = GetComponent<RectTransform>();
+    }
+
+    public virtual void Activate()
+    {
+        PanelCanvas.enabled = true;
+        PanelCanvas.sortingOrder = 1;
+        StopCoroutine("DeactivateWithAnimation");
+        PlayActivateAnimations();
+    }
+
+    public virtual void Deactivate()
+    {
+        StartCoroutine("DeactivateWithAnimation");
+    }
+
+    private IEnumerator DeactivateWithAnimation()
+    {
+        PlayDeactivateAnimations();
+        PanelCanvas.sortingOrder = 2;
+
+        yield return new WaitForSeconds(AnimationTime);
+        PanelCanvas.sortingOrder = 0;
+        PanelCanvas.enabled = false;
+    }
+
+    private void InitUIElements()
+    {
+        for (int i = 0; i < UIElements.Length; i++)
+        {
+            UIElements[i].Init();
+        }
+    }
+
+    public void PlayActivateAnimations()
+    {
+        for (int i = 0; i < UIElements.Length; i++)
+        {
+            UIElements[i].PlayActivateAnimation();
+        }
+    }
+
+    public void PlayDeactivateAnimations()
+    {
+        for (int i = 0; i < UIElements.Length; i++)
+        {
+            UIElements[i].PlayDeactivateAnimation();
+        }
+    }
 }
