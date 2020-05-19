87b6f07eb4a2127a21b74c79d892bb45473c174f
Halil Coşgun <cosgun.halil@gmail.com>
2020-05-19 20:17:23 +0300

    feat: add observer generic base components

---
+++ SuperUI/Assets/SuperUI/Scripts/Helpers/ObserverDesignPattern/IObserver.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LB.SuperUI.Helpers.Observer
+{
+    using System;
+
+    public interface IObserver<in T> where T : EventArgs
+    {
+        void Syncronize(Object sender, T e);
+    }
+}
+
+
---
+++ SuperUI/Assets/SuperUI/Scripts/Helpers/ObserverDesignPattern/ISubject.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LB.SuperUI.Helpers.Observer
+{
+    using System;
+
+    public interface ISubject<out T> where T : EventArgs
+    {
+        void Attach(IObserver<T> observer);
+        void Detach(IObserver<T> observer);
+    }
+}
+
