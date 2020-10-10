221e9bfa30394ee5e5b45dcdad6179b5a6793f70
Halil Coşgun <cosgun.halil@gmail.com>
2020-10-10 18:33:18 +0300

    feat: Add events panel

---
+++ SuperUI/Assets/Sample/Scripts/EventsPanel/EventsPanel.cs
@@ -0,0 +1,27 @@
+﻿using LB.SuperUI.BaseComponents;
+using LB.SuperUI.Helpers.Observer;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsPanel : LB_UIPanel, IObserver<UIStateChangedEventArgs>
+{
+    public void Notify(object sender, UIStateChangedEventArgs e)
+    {
+        panelCanvas.enabled = e.State == UIState.EVENTS;
+    }
+    public override void Setup()
+    {
+        Debug.Log(gameObject.name + "Setup()");
+    }
+
+    protected override void RegisterEvents()
+    {
+        uiManager.Register(this);
+    }
+
+    protected override void UnRegisterEvents()
+    {
+        uiManager.UnRegister(this);
+    }
+}
