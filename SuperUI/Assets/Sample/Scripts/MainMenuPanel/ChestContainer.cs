446f947010dcc3c0fcf1bb2a1f0198428b86ab53
Halil Coşgun <cosgun.halil@gmail.com>
2020-10-17 19:01:49 +0300

    refactor: Remove unnecessary declerations

--- SuperUI/Assets/Sample/Scripts/MainMenuPanel/ChestContainer.cs
+++ SuperUI/Assets/Sample/Scripts/MainMenuPanel/ChestContainer.cs
@@ -6,51 +6,13 @@ using UnityEngine;
 
 public class ChestContainer : LB_UIObject
 {
-    private Vector2 startPosition;
-    private Vector2 targetPosition;
-
     public override void Setup(Vector2 canvasSize)
     {
-        Debug.Log("<color=green>" + gameObject.name + " Setup()</color>");
-
-        startPosition = objectRectTransform.anchoredPosition;
-        targetPosition = new Vector2(
+        activatedCoordinate = objectRectTransform.anchoredPosition;
+        deactivtedCoordinate = new Vector2(
             -canvasSize.x * .5f - objectRectTransform.sizeDelta.x * .5f,
             objectRectTransform.anchoredPosition.y);
 
-    }
-
-    //todo refactor - code duplication
-    public override void PlayActivateAnimation()
-    {
-        if (activationTween != null)
-        {
-            activationTween.Kill();
-        }
-
-        activationTween = objectRectTransform.DOAnchorPos(targetPosition, .5f).SetEase(Ease.InOutSine);
-    }
-
-    public override void PlayDeactivateAnimation()
-    {
-        if (deactivationTween != null)
-        {
-            deactivationTween.Kill();
-        }
-
-        deactivationTween = objectRectTransform.DOAnchorPos(startPosition, .5f);
-    }
-
-    //todo delete
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            PlayActivateAnimation();
-        }
-        else if (Input.GetKeyUp(KeyCode.K))
-        {
-            PlayDeactivateAnimation();
-        }
+        objectRectTransform.anchoredPosition = deactivtedCoordinate;
     }
 }
