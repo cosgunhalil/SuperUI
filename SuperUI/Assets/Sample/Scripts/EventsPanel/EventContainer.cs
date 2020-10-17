15eef3c4738da359c2fca24beb6c657b42ff064e
Halil Coşgun <cosgun.halil@gmail.com>
2020-10-17 19:01:05 +0300

    feat: Add EventContainer script

---
+++ SuperUI/Assets/Sample/Scripts/EventsPanel/EventContainer.cs
@@ -0,0 +1,17 @@
+﻿using LB.SuperUI.BaseComponents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventContainer : LB_UIObject
+{
+    public override void Setup(Vector2 canvasSize)
+    {
+        activatedCoordinate = objectRectTransform.anchoredPosition;
+        deactivtedCoordinate = new Vector2(
+            canvasSize.x * .5f + objectRectTransform.sizeDelta.x * .5f,
+            objectRectTransform.anchoredPosition.y);
+
+        objectRectTransform.anchoredPosition = deactivtedCoordinate;
+    }
+}
