112b9aeeb0714104f9063cb3685e012e4eed3ead
Halil Coşgun <cosgun.halil@gmail.com>
2020-10-18 19:02:33 +0300

    feat: Add playButton

---
+++ SuperUI/Assets/Sample/Scripts/MainMenuPanel/PlayButton.cs
@@ -0,0 +1,16 @@
+﻿using LB.SuperUI.BaseComponents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : LB_Button
+{
+    public override void Setup(Vector2 canvasSize)
+    {
+        activatedCoordinate = objectRectTransform.anchoredPosition;
+        deactivtedCoordinate = new Vector2(0,
+            canvasSize.y * .5f + objectRectTransform.sizeDelta.y * .5f);
+
+        objectRectTransform.anchoredPosition = deactivtedCoordinate;
+    }
+}
