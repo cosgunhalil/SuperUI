58a76fbf72c690363efd6df5c0ba1bc14a430791
Halil Coşgun <cosgun.halil@gmail.com>
2020-10-18 19:03:02 +0300

    refactor: Remove unnecessary implementations

--- SuperUI/Assets/Sample/Scripts/MarketPanel/MarketItem.cs
+++ SuperUI/Assets/Sample/Scripts/MarketPanel/MarketItem.cs
@@ -8,23 +8,11 @@ public class MarketItem : LB_UIObject
 {
     public override void Setup(Vector2 canvasSize)
     {
-        //todo move another place (some main component or ...)
-        animationTime = .5f;
 
         activatedCoordinate = objectRectTransform.anchoredPosition;
         deactivtedCoordinate = new Vector2(
             -canvasSize.x * .5f - objectRectTransform.sizeDelta.x * .5f, 0);
 
         objectRectTransform.anchoredPosition = deactivtedCoordinate;
-    }
-
-    public override void PlayActivateAnimation()
-    {
-        objectRectTransform.DOAnchorPos(activatedCoordinate, animationTime).SetEase(Ease.InOutSine);
-    }
-
-    public override void PlayDeactivateAnimation()
-    {
-        objectRectTransform.DOAnchorPos(deactivtedCoordinate, animationTime).SetEase(Ease.InOutSine);
     }
 }
