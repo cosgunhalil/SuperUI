db5c1bd2682573e7e002f881c2b1be63eb226eaa
Halil Coşgun <cosgun.halil@gmail.com>
2020-10-24 16:50:13 +0300

    refactor(animation): Create animation component

--- SuperUI/Assets/Sample/Scripts/MarketPanel/MarketItem.cs
+++ SuperUI/Assets/Sample/Scripts/MarketPanel/MarketItem.cs
@@ -3,16 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
+using LB.SuperUI.Animation;
 
 public class MarketItem : LB_UIObject
 {
     public override void Setup(Vector2 canvasSize)
     {
+        CreateMoveAnimationComponent(canvasSize);
+    }
 
-        activatedCoordinate = objectRectTransform.anchoredPosition;
-        deactivtedCoordinate = new Vector2(
-            -canvasSize.x * .5f - objectRectTransform.sizeDelta.x * .5f, 0);
+    private void CreateMoveAnimationComponent(Vector2 canvasSize)
+    {
+        LB_MoveAnimationComponent moveAnimationComponent = new LB_MoveAnimationComponent(objectRectTransform, .5f)
+        {
+            AnimationEase = DG.Tweening.Ease.InOutSine,
+            ActivatedCoordinate = objectRectTransform.anchoredPosition,
+            DeactivatedCoordinate = new Vector2(
+                -canvasSize.x * .5f - objectRectTransform.sizeDelta.x * .5f, 0)
 
-        objectRectTransform.anchoredPosition = deactivtedCoordinate;
+        };
+
+        animationComponents.Add(moveAnimationComponent);
+
+        objectRectTransform.anchoredPosition = moveAnimationComponent.DeactivatedCoordinate;
     }
 }
