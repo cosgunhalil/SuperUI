3aeca94e113327e50fb0de5b183d6d4d1b8233b7
Halil Coşgun <cosgun.halil@gmail.com>
2020-10-24 16:45:26 +0300

    feat(animation): Add animation component

---
+++ SuperUI/Assets/SuperUI/Scripts/UIBaseScripts/Animation/LB_AnimationComponent.cs
@@ -0,0 +1,21 @@
+﻿
+namespace LB.SuperUI.Animation 
+{
+    using DG.Tweening;
+    using UnityEngine;
+    public class LB_AnimationComponent 
+    {
+        public Ease AnimationEase { get => animationEase; set => animationEase = value; }
+
+        protected RectTransform objectRectTransform;
+        protected float animationTime;
+        private DG.Tweening.Ease animationEase;
+        public LB_AnimationComponent(RectTransform objectRectTransform, float animationTime) 
+        {
+            this.objectRectTransform = objectRectTransform;
+            this.animationTime = animationTime;
+        }
+    }
+}
+
+
