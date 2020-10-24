2468c1ba1a665400b91d138a60b8eb4ca373c6ce
Halil Coşgun <cosgun.halil@gmail.com>
2020-10-24 16:49:28 +0300

    refactor(animation): Create animation component

--- SuperUI/Assets/Sample/Scripts/CharactersPanel/CharactersMainContainer.cs
+++ SuperUI/Assets/Sample/Scripts/CharactersPanel/CharactersMainContainer.cs
@@ -1,5 +1,7 @@
 ﻿using DG.Tweening;
+using LB.SuperUI.Animation;
 using LB.SuperUI.BaseComponents;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,12 +10,22 @@ public class CharactersMainContainer : LB_UIObject
 {
     public override void Setup(Vector2 canvasSize)
     {
-        activatedCoordinate = objectRectTransform.anchoredPosition;
-        deactivtedCoordinate = new Vector2(
-            canvasSize.x * .5f + objectRectTransform.sizeDelta.x * .5f, 
-            objectRectTransform.anchoredPosition.y);
-
-        objectRectTransform.anchoredPosition = deactivtedCoordinate;
+        CreateMoveAnimationComponent(canvasSize);
     }
 
+    private void CreateMoveAnimationComponent(Vector2 canvasSize)
+    {
+        LB_MoveAnimationComponent moveAnimationComponent = new LB_MoveAnimationComponent(objectRectTransform, .5f)
+        {
+            AnimationEase = Ease.InOutSine,
+            ActivatedCoordinate = objectRectTransform.anchoredPosition,
+            DeactivatedCoordinate = new Vector2(
+            canvasSize.x * .5f + objectRectTransform.sizeDelta.x * .5f,
+            objectRectTransform.anchoredPosition.y)
+        };
+
+        animationComponents.Add(moveAnimationComponent);
+
+        objectRectTransform.anchoredPosition = moveAnimationComponent.DeactivatedCoordinate;
+    }
 }
