e70fa6418cb588b01c600feb1d33f4ce03030fea
Halil Coşgun <cosgun.halil@gmail.com>
2020-10-24 16:47:22 +0300

    feat(animation): Add move animation component

---
+++ SuperUI/Assets/SuperUI/Scripts/UIBaseScripts/Animation/LB_MoveAnimationComponent.cs
@@ -0,0 +1,31 @@
+﻿
+namespace LB.SuperUI.Animation 
+{
+    using DG.Tweening;
+    using UnityEngine;
+    public class LB_MoveAnimationComponent : LB_AnimationComponent, IAnimable
+    {
+        public Vector2 DeactivatedCoordinate { get => deactivatedCoordinate; set => deactivatedCoordinate = value; }
+        public Vector2 ActivatedCoordinate { get => activatedCoordinate; set => activatedCoordinate = value; }
+
+
+        private Vector2 activatedCoordinate;
+        private Vector2 deactivatedCoordinate;
+
+        public LB_MoveAnimationComponent(RectTransform objectRectTransform, float animationTime) : base(objectRectTransform, animationTime)
+        {
+            this.objectRectTransform = objectRectTransform;
+            this.animationTime = animationTime;
+        }
+
+        public void PlayForward()
+        {
+            objectRectTransform.DOAnchorPos(ActivatedCoordinate, animationTime).SetEase(Ease.InOutSine);
+        }
+
+        public void PlayRewind()
+        {
+            objectRectTransform.DOAnchorPos(DeactivatedCoordinate, animationTime).SetEase(Ease.InOutSine);
+        }
+    }
+}
