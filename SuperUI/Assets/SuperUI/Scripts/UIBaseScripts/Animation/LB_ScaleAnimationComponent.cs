7febf91051af9099b662c064f3c420ddca8c0fa7
Halil Coşgun <cosgun.halil@gmail.com>
2020-10-24 16:47:39 +0300

    feat(animation): Add scale animation component

---
+++ SuperUI/Assets/SuperUI/Scripts/UIBaseScripts/Animation/LB_ScaleAnimationComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+namespace LB.SuperUI.Animation 
+{
+    using UnityEngine;
+    using DG.Tweening;
+    public class LB_ScaleAnimationComponent : LB_AnimationComponent, IAnimable
+    {
+        public float ActivatedScale { get => activatedScale; set => activatedScale = value; }
+        public float DeactivatedScale { get => deactivatedScale; set => deactivatedScale = value; }
+
+        private float activatedScale;
+        private float deactivatedScale;
+
+        public LB_ScaleAnimationComponent(RectTransform objectRectTransform, float animationTime) : base(objectRectTransform, animationTime)
+        {
+            this.objectRectTransform = objectRectTransform;
+            this.animationTime = animationTime;
+        }
+
+        public void PlayForward()
+        {
+            objectRectTransform.DOScale(activatedScale, animationTime).SetEase(AnimationEase);
+        }
+
+        public void PlayRewind()
+        {
+            objectRectTransform.DOScale(deactivatedScale, animationTime).SetEase(AnimationEase);
+        }
+    }
+}
+
