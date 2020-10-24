149eb46c02195051819e292e6efc826414d98367
Halil Coşgun <cosgun.halil@gmail.com>
2020-10-24 16:50:05 +0300

    refactor(animation): Create animation component

--- SuperUI/Assets/Sample/Scripts/MainMenuPanel/ChestContainer.cs
+++ SuperUI/Assets/Sample/Scripts/MainMenuPanel/ChestContainer.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using LB.SuperUI.Animation;
 using LB.SuperUI.BaseComponents;
 using UnityEngine;
 
@@ -8,11 +10,36 @@ public class ChestContainer : LB_UIObject
 {
     public override void Setup(Vector2 canvasSize)
     {
-        activatedCoordinate = objectRectTransform.anchoredPosition;
-        deactivtedCoordinate = new Vector2(
+        AddMovementAnimationComponent(canvasSize);
+        AddScaleAnimationComponent();
+    }
+
+    private void AddMovementAnimationComponent(Vector2 canvasSize)
+    {
+        LB_MoveAnimationComponent moveAnimation = new LB_MoveAnimationComponent(objectRectTransform, .5f);
+        moveAnimation.ActivatedCoordinate = objectRectTransform.anchoredPosition;
+        moveAnimation.DeactivatedCoordinate = new Vector2(
             -canvasSize.x * .5f - objectRectTransform.sizeDelta.x * .5f,
             objectRectTransform.anchoredPosition.y);
 
-        objectRectTransform.anchoredPosition = deactivtedCoordinate;
+        objectRectTransform.anchoredPosition = moveAnimation.DeactivatedCoordinate;
+
+        animationComponents.Add(moveAnimation);
+    }
+
+    private void AddScaleAnimationComponent()
+    {
+        LB_ScaleAnimationComponent scaleAnimationComponent = new LB_ScaleAnimationComponent(objectRectTransform, .5f)
+        {
+            ActivatedScale = 1,
+            DeactivatedScale = 0,
+            AnimationEase = Ease.InOutBounce
+        };
+
+        objectRectTransform.localScale = new Vector3(scaleAnimationComponent.DeactivatedScale, 
+            scaleAnimationComponent.DeactivatedScale, 
+            scaleAnimationComponent.DeactivatedScale);
+
+        animationComponents.Add(scaleAnimationComponent);
     }
 }
