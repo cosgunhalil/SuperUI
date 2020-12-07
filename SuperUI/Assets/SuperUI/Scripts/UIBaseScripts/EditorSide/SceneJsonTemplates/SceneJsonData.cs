aded342eb8285731546a77bd89fc8aba0b854e43
Halil Coşgun <cosgun.halil@gmail.com>
2020-12-07 22:04:19 +0300

    feat(editor): Create scene json data object

--- SuperUI/Assets/SuperUI/Scripts/UIBaseScripts/EditorSide/SceneJsonTemplates/SceneJsonData.cs
+++ SuperUI/Assets/SuperUI/Scripts/UIBaseScripts/EditorSide/SceneJsonTemplates/SceneJsonData.cs
@@ -2,12 +2,59 @@
 namespace LB.SuperUI.Editor 
 {
     using System;
+    using System.Collections.Generic;
     [Serializable]
     public class SceneJsonData
     {
         public string UIManagerName;
-        public PanelJsonData[] panelJsonData;
+        public List<PanelJsonData> panelDataList;
 
+        public SceneJsonData() 
+        {
+            UIManagerName = "UIManager";
+            panelDataList = new List<PanelJsonData>();
+        }
+
+        public void AddPanelData(PanelJsonData panelJsonData)
+        {
+            panelDataList.Add(panelJsonData);
+        }
+
+        public void RemovePanelData(string panelName) 
+        {
+            for (int i = 0; i < panelDataList.Count; i++)
+            {
+                if (panelDataList[i].Name == panelName) 
+                {
+                    panelDataList.RemoveAt(i);
+                }
+            }
+        }
+
+        public void RemovePanelData(int panelIndex) 
+        {
+            if (panelIndex >= panelDataList.Count) 
+            {
+                return;
+            }
+
+            if (panelIndex < 0) 
+            {
+                return;
+            }
+
+            panelDataList.RemoveAt(panelIndex);
+        }
+
+        public void ClearPanelData() 
+        {
+            panelDataList.Clear();
+        }
+
+        public List<PanelJsonData> GetPanelDataList()
+        {
+            return panelDataList;
+        }
     }
 }
 
