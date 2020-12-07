b27837e6a710463c39d4afb8c21337fa9269f6ab
Halil Coşgun <cosgun.halil@gmail.com>
2020-12-07 22:03:12 +0300

    feat(editor): Create panel json data

--- SuperUI/Assets/SuperUI/Scripts/UIBaseScripts/EditorSide/SceneJsonTemplates/PanelJsonData.cs
+++ SuperUI/Assets/SuperUI/Scripts/UIBaseScripts/EditorSide/SceneJsonTemplates/PanelJsonData.cs
@@ -2,12 +2,55 @@
 namespace LB.SuperUI.Editor 
 {
     using System;
+    using System.Collections.Generic;
 
     [Serializable]
     public class PanelJsonData
     {
         public string Name;
-        public UIObjectJsonData[] uIObjectJsonDatas;
+        public List<UIObjectJsonData> uiObjectDataList;
+
+        public PanelJsonData() 
+        {
+            uiObjectDataList = new List<UIObjectJsonData>();
+        }
+
+        public void AddUIObject(UIObjectJsonData uIObjectJsonData)
+        {
+            uiObjectDataList.Add(uIObjectJsonData);
+        }
+
+        public void RemoveUIObject(string uiObjectName) 
+        {
+            for (int i = 0; i < uiObjectDataList.Count; i++)
+            {
+                if (uiObjectDataList[i].Name == uiObjectName) 
+                {
+                    uiObjectDataList.RemoveAt(i);
+                }
+            }
+        }
+
+        //TODO: write generic function!!!
+        public void RemoveUIObject(int index)
+        {
+            if (index >= uiObjectDataList.Count)
+            {
+                return;
+            }
+
+            if (index < 0)
+            {
+                return;
+            }
+
+            uiObjectDataList.RemoveAt(index);
+        }
+
+        public List<UIObjectJsonData> GetUIObjectList() 
+        {
+            return uiObjectDataList;
+        }
     }
 }
 
