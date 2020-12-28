0ed999ad91a4dda2105e525a11978dcf805ab0fa
Halil Coşgun <cosgun.halil@gmail.com>
2020-12-28 22:02:27 +0300

    feat(file): Write file loader component

---
+++ SuperUI/Assets/Helpers/FileHandler/LB_FileLoader.cs
@@ -0,0 +1,46 @@
+﻿
+namespace LB.Helper.FileHandler 
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Runtime.Serialization.Formatters.Binary;
+    using UnityEngine;
+
+    public class LB_Loader
+    {
+        public T Load<T>(string path)
+        {
+            var data = ReadDataFromPath(path);
+            return JsonUtility.FromJson<T>(data);
+        }
+
+        public string ReadDataFromPath(string path)
+        {
+            string data = null;
+            try
+            {
+                using (FileStream fileStream = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        var binaryFormatter = new BinaryFormatter();
+                        var dataString = binaryFormatter.Deserialize(fileStream) as string;
+
+                        data = reader.ReadToEnd();
+
+                        return dataString;
+                    }
+                }
+            }
+            catch (System.Exception exception)
+            {
+                Debug.LogError(exception);
+            }
+
+            return data;
+        }
+    }
+
+}
+
